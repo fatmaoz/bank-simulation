58135ba5d48d5ec52a0f2e957dd0046adc49c7c6
fatma1990-jd <fatma@cybertekschool.com>
2022-06-01 15:56:56 +0100

    added security

---
+++ src/main/java/com/cydeo/banksimulation/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.cydeo.banksimulation.config;
+
+import com.cydeo.banksimulation.service.SecurityService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    //private final SecurityService securityService;  ????????????????????
+    private final AuthSuccessHandler authSuccessHandler;
+
+    public SecurityConfig(AuthSuccessHandler authSuccessHandler) {
+       // this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeRequests()
+//                .antMatchers("/user/**").hasRole("Admin")
+                .antMatchers("/account/**").hasAuthority("Admin")
+                //.antMatchers("/transaction/**").hasAuthority("Admin")
+                .antMatchers("/transaction/**").hasAnyAuthority("Admin","Manager")
+//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
+//                .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")
+
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/assets/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                   // .defaultSuccessUrl("/index")
+                .successHandler(authSuccessHandler)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+//                .and()
+//                .rememberMe()
+//                .tokenValiditySeconds(120)
+//                .key("cydeo")
+//                .userDetailsService((UserDetailsService) securityService)
+                .and().build();
+
+    }
+
+}
---
+++ src/main/java/com/cydeo/banksimulation/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cydeo.banksimulation.repository;
+
+import com.cydeo.banksimulation.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+}
